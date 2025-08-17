639c948efc75b22c438be529a6adf580b606f4cb
Georgechrp <christopoulosg2@gmail.com>
2025-08-17 19:33:10 +0300

    Remove MovieGenreScreen, VM, and images

--- app/src/main/java/com/christopoulos/moviesearch/presentation/navigation/Destinations.kt
+++ app/src/main/java/com/christopoulos/moviesearch/presentation/navigation/Destinations.kt
@@ -2,9 +2,8 @@ package com.christopoulos.moviesearch.presentation.navigation
 
 object Destination {
     object Splash { const val route = "splash" }
-    object MovieGenres { const val route = "movies_genres" }
 
-    // Αναζήτηση (κύρια λίστα αναζήτησης)
+
     object Search { const val route = "search" }
 
     // Λεπτομέρειες με movieId
